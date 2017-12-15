abaf46d66c56b20e7370bb207e1dc698263f2b07
raika44 <33775480+raika44@users.noreply.github.com>
2017-12-15 09:30:11 +0700

    Update aan.py

--- aan.py
+++ aan.py
@@ -4,6 +4,7 @@
 import LINETCR
 from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
+from bs4 import BeautifulSoup
 from threading import Thread
 from googletrans import Translator
 from gtts import gTTS
